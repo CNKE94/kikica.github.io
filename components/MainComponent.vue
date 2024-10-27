72917edfff826383cd1a1d69161e61bd62ef9bf6
Ivan Veljkovic <ivan.veljkovic@mdpi.com>
2024-10-27 22:01:42 +0100

    Adjustment of heading at land page

--- components/MainComponent.vue
+++ components/MainComponent.vue
@@ -12,10 +12,10 @@ const details = {
         src="/images/vencanje.jpeg"
         alt="alt"
     />
-    <div class="absolute top-1/2 left-1/2 translate-x-[-50%] text-center w-full">
-        <AppHeading class="text-8xl" heading="h1" :title="details.title" />
-        <AppHeading class="text-5xl mt-5" heading="h2" :title="details.description" />
-        <p class="w-3/4 mx-auto text-4xl mt-14 pt-14 border-t-4 border-orange-500">Subota, 31 Maj 2025</p>
+    <div class="absolute top-1/3 lg:top-1/2 left-1/2 translate-x-[-50%] text-center w-full">
+        <AppHeading class="text-6xl lg:text-8xl" heading="h1" :title="details.title" />
+        <AppHeading class="text-3xl lg:text-5xl mt-5" heading="h2" :title="details.description" />
+        <p class="w-3/4 mx-auto text-2xl lg:text-4xl mt-14 pt-14 border-t-4 border-orange-500">Subota, 31 Maj 2025</p>
     </div>
   </div>
 </template>
