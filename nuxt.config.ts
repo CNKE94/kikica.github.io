9d9a4b04ca925a0be7364d3301d0565afb01d73a
Ivan Veljkovic <ivan.veljkovic@mdpi.com>
2024-10-27 18:11:28 +0100

    fasdf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   app: {
-    baseURL: 'https://github.com/CNKE94/kikica.github.io.git',
+    baseURL: '/kikica.github.io/',
   },
 })
