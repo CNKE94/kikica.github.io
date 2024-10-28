569680bfc1c664091333a8467a4e11afbf060a25
Ivan Veljkovic <ivan.veljkovic@mdpi.com>
2024-10-28 21:22:28 +0100

    add title ant stuff

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/images/wedding-couple.svg",
+    },
+  ],
+});
+
+useSeoMeta({
+  title: "Кристина & Иван",
+  charset: "utf-8",
+  viewport: "width=device-width, initial-scale=1",
+
+})
+</script>
+
 <template>
   <div>
     <NuxtLayout>
