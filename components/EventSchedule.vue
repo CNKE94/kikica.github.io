50f4a289af336721eed8df6af221e8fd0ed62ee6
Ivan Veljkovic <ivan.veljkovic@mdpi.com>
2024-10-28 09:30:58 +0100

    correct date

--- components/EventSchedule.vue
+++ components/EventSchedule.vue
@@ -26,7 +26,7 @@ const details = [
     <AppContainer class="flex flex-col bg-orange-200">
         <div class="text-center pb-10 mb-10 border-b-2 border-orange-500">
             <p>Распоред догађаја</p>
-            <AppHeading class="text-3xl mt-3" heading="h2" title="Недеља, 31. Мај 2025"></AppHeading>
+            <AppHeading class="text-3xl mt-3" heading="h2" title="Субота, 31. Мај 2025" />
         </div>
         <div class="grid gap-5 sm:grid-cols-1 lg:grid-cols-2">
             <AppCard
