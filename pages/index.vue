812044edd67f09dc5978e929daf77f0f91cd02ff
Ivan Veljkovic <ivan.veljkovic@mdpi.com>
2024-10-29 11:45:24 +0100

    scroll emit

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const audio = ref<HTMLAudioElement | null>(null);
+
+const playAudio = () => {
+  if (audio.value) {
+    audio.value.muted = true;
+    audio.value.play().then(() => {
+      audio.value!.muted = false;
+    }).catch(error => {
+      console.error("Error playing audio:", error);
+    });
+  }
+};
+</script>
 
 <template>
   <div>
@@ -8,5 +21,11 @@
     <FooterComponent />
     <SlideShowComponent />
     <AppSound />
+  </div>
+  <div @scroll="playAudio">
+    <audio
+      ref="audio"
+      src="/kristinaivanvencanje/sound/little-things.mp3"
+    />
   </div>
 </template>
